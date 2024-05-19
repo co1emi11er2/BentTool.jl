919cb700b1e9d9326ab5eeec2cb6e5f74a053060
Documenter.jl <documenter@juliadocs.github.io>
2024-05-19 00:03:07 +0000

    build based on c868385

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BentTool","category":"page"},{"location":"#BentTool","page":"Home","title":"BentTool","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BentTool.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BentTool]","category":"page"}]
+}
